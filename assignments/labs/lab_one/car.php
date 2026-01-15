b2c1eef5de3c65f9eb733061bd91a139e00b92d4
RonaldGon <ronaldalegonzalez@gmail.com>
2026-01-15 10:18:58 -0500

    Finish class

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -1,3 +1,26 @@
 <?php
 
+class Car {
+
+public string $make;
+public string $model;
+public int $year;
+
+public function __construct(string $make, string $model, int $year){
+    $this->make = $make;
+    $this->model = $model;
+    $this->year = $year;
+}
+public function _Info() : string{
+    return "Car Make: {$this->make} | Car Model : {$this->model} | Car Year: {$this->year}";
+}
+
+
+
+
+
+
+
+
+}
 
--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -2,3 +2,4 @@
 require "header.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
+
