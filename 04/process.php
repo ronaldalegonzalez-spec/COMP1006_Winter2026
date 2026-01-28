69a690a24eeb1730826d571f345bf101eb985b09
RonaldGon <ronaldalegonzalez@gmail.com>
2026-01-28 15:49:14 -0500

    try by my own the process.php

--- 04/process.php
+++ 04/process.php
@@ -22,5 +22,5 @@ echo"<p> Thank you for your submitting !</p>";
 //What I did -->
 echo"<p> Here is a summary of your order:</p>";
 
-echo "<p>Your Name is </p>" . htmlspecialchars($_POST['first_name']) . " " . htmlspecialchars($_POST['last_name']) . ".";
-echo "Your phone Number " . (int) $_POST['phone'] . " years old.";
+echo "<p>Your Name is: " . ($_POST['first_name']) . " " . htmlspecialchars($_POST['last_name']) . "." . "</p>";
+echo "<p>Your phone number is: " . (int) $_POST['phone'] . "." . "</p>";
