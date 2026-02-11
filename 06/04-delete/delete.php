edeee986fb750303dd061fdf23a129b3e4ddb569
RonaldGon <ronaldalegonzalez@gmail.com>
2026-02-11 12:04:56 -0500

    addedmlesson 6

---
+++ 06/04-delete/delete.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * delete.php
+ * ------------------------------------------------------------
+ * Deletes one order from the orders1 table.
+ * - Gets the customer_id from the URL (delete.php?id=5)
+ * - Uses PDO + bindParam for safety
+ * - Redirects back to orders.php
+ */
+
+
+// make sure we received an ID
+// create the query 
+//prepare 
+//bind 
+//execute 
+
+// Redirect back to admin list
+header("Location: orders.php");
+exit;
